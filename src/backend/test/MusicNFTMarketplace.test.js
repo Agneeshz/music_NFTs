97bc3124f0e003b208ca604b3e8279c46bdbca94
Agneeshz <84716985+Agneeshz@users.noreply.github.com>
2022-06-11 17:30:13 +0530

    backend work

--- src/backend/test/MusicNFTMarketplace.test.js
+++ src/backend/test/MusicNFTMarketplace.test.js
@@ -4,7 +4,7 @@ const { ethers } = require("hardhat");
 const toWei = (num) => ethers.utils.parseEther(num.toString())
 const fromWei = (num) => ethers.utils.formatEther(num)
 
-describe("MusicNFTMarketplace",function() 
+describe("MusicNFTMarketplace",function()  // describe() is simply a way to group our tests in Mocha. We can nest our tests in groups as deep as we deem necessary. describe() takes two arguments, the first is the name of the test group, and the second is a callback function.
 {
     let nftMarketplace
     let deployer, artist, user1, user2, users;
@@ -38,6 +38,68 @@ describe("MusicNFTMarketplace",function()
         expect(await nftMarketplace.name()).to.equal(nftName);
         expect(await nftMarketplace.symbol()).to.equal(nftSymbol);
         expect(await nftMarketplace.baseURI()).to.equal(URI);
+        expect(await nftMarketplace.royaltyFee()).to.equal(royaltyFee);
+        expect(await nftMarketplace.artist()).to.equal(artist.address);
+      });
+
+      it("Should mint them list all the music nfts", async function ()
+      {
+        expect(await nftMarketplace.balanceOf(nftMarketplace.address)).to.equal(8);
+        // Get each item from the marketItems array then check fields to ensure they are correct
+        await Promise.all(prices.map(async (i, indx) =>
+        {
+          const item = await nftMarketplace.marketItems(indx)
+          expect(item.tokenID).to.equal(indx)
+          expect(item.seller).to.equal(deployer.address)
+          expect(item.price).to.equal(i)
+        }));
+      });
+      it("Ether balance should equal deployment fees", async function()
+      {
+        expect(await ethers.provider.getBalance(nftMarketplace.address)).to.equal(deploymentFees)
       });
     });
+    describe("Updating royalty fee", function()
+    {
+      it("Only deployer should be able to update royalty fee", async function()
+      {
+        const fee = toWei(0.02)
+        await nftMarketplace.updateRoyaltyFee(fee)
+        await expect(
+          nftMarketplace.connect(user1).updateRoyaltyFee(fee)
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        expect(await nftMarketplace.royaltyFee()).to.equal(fee);
+      });
+    });
+    describe("Buying tokens", function(){
+      it("Should update seller to zero address, transfer NFT, pay seller, pay royalty to artist and emit a MarketItemBought event", async function () {
+        const deployerInitalEthBal = await deployer.getBalance()
+        const artistInitialEthBal = await artist.getBalance()
+        // user1 purchases item.
+        await expect(nftMarketplace.connect(user1).buyToken(0, { value: prices[0] }))
+          .to.emit(nftMarketplace, "MarketItemBought")
+          .withArgs(
+            0,
+            deployer.address,
+            user1.address,
+            prices[0]
+          )
+        const deployerFinalEthBal = await deployer.getBalance()
+        const artistFinalEthBal = await artist.getBalance()
+        // Item seller should be zero addr
+        expect((await nftMarketplace.marketItems(0)).seller).to.equal("0x0000000000000000000000000000000000000000")
+        // Seller should receive payment for the price of the NFT sold.
+        expect(+fromWei(deployerFinalEthBal)).to.equal(+fromWei(prices[0]) + +fromWei(deployerInitalEthBal))
+        // Artist should receive royalty
+        expect(+fromWei(artistFinalEthBal)).to.equal(+fromWei(royaltyFee) + +fromWei(artistInitialEthBal))
+        // The buyer should now own the nft
+        expect(await nftMarketplace.ownerOf(0)).to.equal(user1.address);
+      })
+      it("Should fail when ether amount sent with transaction does not equal asking price", async function(){
+        // Fails when ether sent does not equal asking price
+        await expect(
+          nftMarketplace.connect(user1).buyToken(0, { value: prices[1] })
+        ).to.be.revertedWith("Please send the asking price in order to complete the purchase");
+      })
+    })
 })
